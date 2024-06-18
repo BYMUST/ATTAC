aef9a744f3398c1b96a00f34053363a5e657b5b3
BYMUST <92484638+BYMUST@users.noreply.github.com>
2024-06-18 15:44:02 +0800

    olwf_asym_original.py

--- src/approach/olwf_asym_original.py
+++ src/approach/olwf_asym_original.py
@@ -301,7 +301,7 @@ class Appr(Inc_Learning_Appr):
     def pod(self,
         list_attentions_a,
         list_attentions_b,
-        collapse_channels="channels",
+        collapse_channels="spatial",
         normalize=True
     ):
         """Pooled Output Distillation.
@@ -373,11 +373,13 @@ class Appr(Inc_Learning_Appr):
                 diff = a-b
                 relu_out = asym_choice(diff)  
                 layer_loss = torch.mean(torch.frobenius_norm(relu_out, dim=-1))
+                print("ReLU")
             else:
                 if normalize:
                     a = F.normalize(a, dim=1, p=2)
                     b = F.normalize(b, dim=1, p=2)
                 layer_loss = torch.mean(torch.frobenius_norm(a - b, dim=-1))
+                print("Not ReLU")
             loss += layer_loss 
 
         return loss / len(list_attentions_a)
